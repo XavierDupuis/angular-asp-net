0722127cdd5a9d9915db402bf34a08172beb76ed
XavierDupuis <github-xavierdupuis@outlook.com>
2024-09-02 12:28:24 -0400

    use jest and spectator

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -1,31 +1,25 @@
-import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { Spectator, createComponentFactory } from '@ngneat/spectator';
 
 describe('AppComponent', () => {
+    let component: AppComponent;
+    let spectator: Spectator<AppComponent>;
+    const createComponent = createComponentFactory(AppComponent);
+
     beforeEach(async () => {
-        await TestBed.configureTestingModule({
-            imports: [AppComponent],
-        }).compileComponents();
+        spectator = createComponent();
+        component = spectator.component;
     });
 
     it('should create the app', () => {
-        const fixture = TestBed.createComponent(AppComponent);
-        const app = fixture.componentInstance;
-        expect(app).toBeTruthy();
+        expect(component).toBeTruthy();
     });
 
     it(`should have the 'client' title`, () => {
-        const fixture = TestBed.createComponent(AppComponent);
-        const app = fixture.componentInstance;
-        expect(app.title).toEqual('client');
+        expect(component.title).toEqual('client');
     });
 
     it('should render title', () => {
-        const fixture = TestBed.createComponent(AppComponent);
-        fixture.detectChanges();
-        const compiled = fixture.nativeElement as HTMLElement;
-        expect(compiled.querySelector('h1')?.textContent).toContain(
-            'Hello, client'
-        );
+        expect(spectator.query('h1')?.textContent).toContain('Hello, client');
     });
 });
